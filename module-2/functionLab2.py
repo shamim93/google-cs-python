c24d7a7e6ee9d31706cae7c39bd4624b7be10a41
Shamim Hossen <shamim41.cse@gmail.com>
2023-11-18 21:31:53 +0600

    function Lab 2

---
+++ module-2/functionLab2.py
@@ -0,0 +1,29 @@
+# Define a function named `analyze_logins()` that takes in three parameters, `username`, `current_day_logins`, and `average_day_logins`
+
+def analyze_logins(username, current_day_logins, average_day_logins):
+
+    # Display a message about how many login attempts the user has made that day
+
+    print("Current day login total for", username, "is", current_day_logins)
+
+    # Display a message about average number of login attempts the user has made that day
+
+    print("Average logins per day for", username, "is", average_day_logins)
+
+    # Calculate the ratio of the logins made on the current day to the logins made on an average day, storing in a variable named `login_ratio`
+
+    login_ratio = current_day_logins / average_day_logins
+
+    # Return the ratio
+
+    return login_ratio
+
+# Call `analyze_logins() and store the output in a variable named `login_analysis`
+
+
+login_analysis = analyze_logins("ejones", 9, 5)
+
+# Conditional statement that displays an alert about the login activity if it's more than normal
+
+if login_analysis >= 3:
+    print("Alert! This account has more login activity than normal.")
