70b90133086d4bbe2427c484e7e6a1bab48a2ab4
Shamim Hossen <shamim41.cse@gmail.com>
2023-11-26 21:53:15 +0600

    String manipulation lab

--- module-3/stringLab1.py
+++ module-3/stringLab1.py
@@ -1,6 +1,7 @@
 # Task 1
 # convert a number to string
 # Assign `employee_id` to a four digit number as an initial value
+'''
 
 employee_id = 4186
 
@@ -15,3 +16,29 @@ employee_id = str(employee_id)
 # Display the data type of `employee_id` now
 
 print(type(employee_id))
+
+
+'''
+# Task 2
+#  Conditional statement to employee id,write a conditional statement that displays a message if the length of the employee ID is less than five digits.
+'''
+employee_id = 4186
+print(type(employee_id))
+employee_id = str(employee_id)
+
+if len(employee_id) < 5:
+    employee_id = employee_id + "E"
+
+print(employee_id)
+'''
+# Task 5
+
+# need to extract the first through the third characters in the device ID. So take a slice of the device ID.
+
+# Assign `device_id` to a string that contains alphanumeric characters
+
+device_id = "r262c36"
+
+# Extract the first through the third characters in `device_id` and display the result
+
+print(device_id[0:3])
