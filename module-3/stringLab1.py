453a4338bc3f0af6b04bbf65fc2ab61a206cee37
Shamim Hossen <shamim41.cse@gmail.com>
2023-11-26 17:32:34 +0600

    module 3, string operation

---
+++ module-3/stringLab1.py
@@ -0,0 +1,17 @@
+# Task 1
+# convert a number to string
+# Assign `employee_id` to a four digit number as an initial value
+
+employee_id = 4186
+
+# Display the data type of `employee_id`
+
+print(type(employee_id))
+
+# Reassign `employee_id` to the same value but in the form of a string
+
+employee_id = str(employee_id)
+
+# Display the data type of `employee_id` now
+
+print(type(employee_id))
