77d20d3428cf55722cce88bab25d63e2c35a6e05
Shamim Hossen <shamim41.cse@gmail.com>
2023-11-27 09:08:17 +0600

    Develop an algorithm Lab 1 complete

--- module-3/developAnAlgoLab.py
+++ module-3/developAnAlgoLab.py
@@ -63,6 +63,8 @@ print(ind)
 '''
 # Task 6
 #  Get the index of the username and devices
+
+'''
 approved_users = ["elarson", "bmoreno", "sgilmore", "eraab", "gesparza"]
 approved_devices = ["8rp2k75", "hl0s5o1", "4n482ts", "a307vir", "3rcv4w6"]
 username = "sgilmore"
@@ -70,3 +72,52 @@ ind = approved_users.index(username)
 # the devices of that index
 assignedDevice = approved_devices[ind]
 print(assignedDevice)
+
+'''
+# Task 7
+# Conditional statement to compare approved devices and username
+'''
+
+approved_users = ["elarson", "bmoreno", "sgilmore", "eraab", "gesparza"]
+approved_devices = ["8rp2k75", "hl0s5o1", "4n482ts", "a307vir", "3rcv4w6"]
+username = "sgilmore"
+device_id = "a307vir"
+ind = approved_users.index(username)
+
+# Conditional statement
+# If `username` belongs to `approved_users`, and if the device ID at `ind` in `approved_devices` matches `device_id`,
+# then display a message that the username is approved,
+# followed by a message that the user has the correct device
+
+if username in approved_users and device_id == approved_devices[ind]:
+    print("The username", username, "is approved to access the system.")
+    print(device_id, "is the assigned device for", username)
+
+elif username in approved_users and device_id != approved_devices[ind]:
+    print("The user", username, "is approved to access the system, but",
+          device_id, "is not their assigned device.")
+
+'''
+
+#  task 9
+#  create a function to check username and assigned devices
+
+approved_users = ["elarson", "bmoreno", "sgilmore", "eraab", "gesparza"]
+approved_devices = ["8rp2k75", "hl0s5o1", "4n482ts", "a307vir", "3rcv4w6"]
+
+
+def login(username, device_id):
+    if username in approved_users:
+        print("The user", username, "is approved to access the system.")
+        ind = approved_users.index(username)
+        if device_id == approved_devices[ind]:
+            print(device_id, "is the assigned device for", username)
+        else:
+            print(device_id, "is not their assigned device.")
+
+    else:
+        print("The username", username, "is not approved to access the system.")
+
+
+login("bmoreno", "hl0s5o1")
+login("bmoreno", "4n482ts")
