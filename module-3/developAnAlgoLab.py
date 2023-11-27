02d29d1fe88054edf1fe9f27c003c56b8ab6ff81
Shamim Hossen <shamim41.cse@gmail.com>
2023-11-27 08:39:27 +0600

    Develop an algorithm Lab 1

--- module-3/developAnAlgoLab.py
+++ module-3/developAnAlgoLab.py
@@ -1,6 +1,7 @@
 # Task 2
 # Append new items to list
 
+'''
 # Assign `approved_users` to a list of approved usernames
 approved_users = ["elarson", "bmoreno", "tshah", "sgilmore", "eraab"]
 # Assign `approved_devices` to a list of device IDs that correspond to the usernames in `approved_users`
@@ -16,3 +17,56 @@ approved_devices.append(new_device)
 print(approved_users)
 # Diplay the contents of `approved_devices`
 print(approved_devices)
+
+'''
+# Task 3
+#  Remove users and devices from approved list
+
+'''
+approved_users = ["elarson", "bmoreno",
+                  "tshah", "sgilmore", "eraab", "gesparza"]
+approved_devices = ["8rp2k75", "hl0s5o1",
+                    "2ye3lzg", "4n482ts", "a307vir", "3rcv4w6"]
+removed_user = "tshah"
+removed_device = "2ye3lzg"
+approved_users.remove(removed_user)
+approved_devices.remove(removed_device)
+print(approved_users)
+print(approved_devices)
+
+'''
+
+# Task 4
+# Write a conditional statement that verifies if a given username is an element of the list of approved usernames.
+'''
+approved_users = ["elarson", "bmoreno", "sgilmore", "eraab", "gesparza"]
+approved_devices = ["8rp2k75", "hl0s5o1", "4n482ts", "a307vir", "3rcv4w6"]
+username = "shamim"
+
+if username in approved_users:
+    print("The username", username, "is approved to access the system.")
+else:
+    print("The user", username, "is not approved to access the system.")
+
+    
+'''
+# Task 5
+# The next part of the algorithm uses the .index() method to find the index of username in the approved_users and store that index in a variable named ind.
+# When used on a list, the .index() method will return the position of the given value in the list.
+
+'''
+approved_users = ["elarson", "bmoreno", "sgilmore", "eraab", "gesparza"]
+approved_devices = ["8rp2k75", "hl0s5o1", "4n482ts", "a307vir", "3rcv4w6"]
+username = "sgilmore"
+ind = approved_users.index(username)
+print(ind)
+'''
+# Task 6
+#  Get the index of the username and devices
+approved_users = ["elarson", "bmoreno", "sgilmore", "eraab", "gesparza"]
+approved_devices = ["8rp2k75", "hl0s5o1", "4n482ts", "a307vir", "3rcv4w6"]
+username = "sgilmore"
+ind = approved_users.index(username)
+# the devices of that index
+assignedDevice = approved_devices[ind]
+print(assignedDevice)
