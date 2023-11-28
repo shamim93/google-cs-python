3620f04926f9159b22403cba7fb22fa9cb5a4e34
Shamim Hossen <shamim41.cse@gmail.com>
2023-11-28 11:16:08 +0600

    Create a new file and write on it

--- module-4/writeFile.py
+++ module-4/writeFile.py
@@ -1,3 +1,4 @@
+'''
 with open("test.txt", "a") as file:
     newLineAdd = file.write("\nNew text has been added to an existing file")
     file.close()
@@ -5,3 +6,14 @@ with open("test.txt", "r") as newf:
     newFile = newf.read()
 print(newFile)
 file.close()
+
+'''
+
+# creat a new file and write on it
+
+with open("test2.txt", "w") as file:
+    newFile = file.write("Hello World!")
+    file.close()
+with open("test2.txt", "r") as file:
+    readFile = file.read()
+print(readFile)
